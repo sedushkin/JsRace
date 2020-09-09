640e03b64604fa021ef10e362d83d343a488be96
Sedushkin Vladimir <sedushkin1@yandex.ru>
2020-09-09 17:52:18 +0300

    Выполнение второго урока

--- main.js
+++ main.js
@@ -22,30 +22,51 @@ const setting = {
     start: false,
     score: 0,
     speed: 3
+    
 };
 
 function startGame(){
     start.classList.add('hide');
     setting.start = true;
     gameArea.appendChild(car);
+    setting.x = car.offsetLeft;
+    setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
 }
 
 function playGame(){
     console.log('Play Game!')
     if (setting.start) {
+        if (keys.ArrowLeft && setting.x>0) {
+            setting.x -= setting.speed;
+        }
+        if (keys.ArrowRight && setting.x<(gameArea.offsetWidth - 50)) {
+            setting.x += setting.speed;
+        }
+        if (keys.ArrowDown && setting.y<(gameArea.offsetHight - 50)) {
+            setting.y += setting.speed;
+        }
+        if (keys.ArrowUp && setting.y>0) {
+            setting.y -= setting.speed;
+        }
+
+
+
+        car.style.left = setting.x + 'px';
+        car.style.top = setting.y + 'px';
          requestAnimationFrame(playGame);
     } 
 }
 
 function startRun(event){
-    event.preventDefault();
-    keys[event.key] = true;
+    if (event.key !== 'F5' && event.key !== 'F12') {
+        event.preventDefault();
+        keys[event.key] = true;
+    }
+    
 }
 
 function stopRun(event){
     event.preventDefault();
     keys[event.key] = false;
 }
-
-
